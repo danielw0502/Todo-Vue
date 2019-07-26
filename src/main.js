1b844dc4f54f5dfa51858f4441ea6b9bd5c9736a
Xianda Wang <wxd920502@gmail.com>
2019-07-26 20:21:32 +0800

    04-vuex

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 
+
+import { store } from './store/store'
+
 window.eventBus = new Vue()
 
 Vue.config.productionTip = false
@@ -10,6 +13,7 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store: store,
   components: { App },
   template: '<App/>'
 })
